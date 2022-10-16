dd17a468bc8b926a672ff0ca623bb563aefbee37
Odin Kroeger <21236809+odkr@users.noreply.github.com>
2022-10-16 13:52:49 +0200

    tests: more tests for file_is_exec.

--- tests/file_is_exec.sh
+++ tests/file_is_exec.sh
@@ -31,36 +31,38 @@ fname="$TMPDIR/file"
 touch "$fname"
 chown "$euid:$egid" "$fname"
 
-no="ugo= ug=,o=x"
+no="ugo= uo=,g=x ug=,o=x"
 for mode in $no
 do
+	warn "checking $mode with owner $euid and group $egid ..."
 	chmod "$mode" "$fname"
 	file_is_exec "$fname" &&
-		err "file_is_exec reports $mode as executable."
+		err "reported as executable."
 	chmod ugo= "$fname"
 done
 
-yes="ugo=x ug=x u=x g=x"
+yes="u=x ug=x ugo=x"
 for mode in $yes
 do
+	warn "checking $mode with owner $euid and group $egid ..."
 	chmod "$mode" "$fname"
 	file_is_exec "$fname" ||
-		err "file_is_exec does not report $mode as executable."
+		err "not reported as executable."
 	chmod ugo= "$fname"
 done
 
+warn "checking /bin/sh ..."
 file_is_exec /bin/sh ||
-	err "file_is_exec does not report /bin/sh as executable."
-
-
-#
-# Interlude
-#
+	err "not reported as executable."
 
 euid="$(id -u)" && [ "$euid" ] ||
 	err "failed to get process' effective user ID."
 
-[ "$euid" -ne 0 ] && exit
+if [ "$euid" -ne 0 ]
+then
+	warn "${green}success.$reset"
+	exit
+fi
 
 unalloc_uid="$(unallocid -u 1000 30000)" && [ "$unalloc_uid" ] ||
 	err "failed to find an unallocated user ID."
@@ -69,10 +71,48 @@ unalloc_gid="$(unallocid -g 1000 30000)" && [ "$unalloc_gid" ] ||
 
 chown "$unalloc_uid:$unalloc_gid" "$fname"
 
+warn "checking o= with owner $unalloc_uid and group $unalloc_gid ..."
 chmod o= "$fname"
 file_is_exec "$fname" &&
-	err "file_is_exec reports $mode as executable."
+	err "reported as executable."
 
+warn "checking o=x with owner $unalloc_uid and group $unalloc_gid ..."
 chmod o=x "$fname"
 file_is_exec "$fname" ||
-	err "file_is_exec does not report $mode as executable."
+	err "not reported as executable."
+
+warn "checking u=x with owner $euid and group $unalloc_gid ..."
+chown "$euid" "$fname"
+chmod u=x,go= "$fname"
+file_is_exec "$fname" ||
+        err "not reported as executable."
+
+warn "checking u=x with owner $unalloc_uid and group $egid ..."
+chown "$unalloc_uid:$egid" "$fname"
+file_is_exec "$fname" &&
+        err "reported as executable."
+
+warn "checking g=x with owner $euid and group $unalloc_gid ..."
+chown "$euid:$unalloc_gid" "$fname"
+chmod uo=,g=x "$fname"
+file_is_exec "$fname" &&
+        err "reported as executable."
+
+warn "checking g=x with owner $unalloc_uid and group $egid ..."
+chown "$unalloc_uid:$egid" "$fname"
+file_is_exec "$fname" ||
+        err "not reported as executable."
+
+warn "checking g=x with owner $euid and group $egid ..."
+chown "$euid:$egid" "$fname"
+file_is_exec "$fname" &&
+        err "reported as executable."
+
+ruid="$(regularuid)" && [ "$ruid" ] ||
+	err "failed to get non-root user ID of caller."
+rgid="$(id -g "$ruid")" && [ "$rgid" ] ||
+	err "failed to get primary group ID of UID $ruid"
+
+runas "$ruid" "$rgid" "$0"
+
+warn "${green}success.$reset"
