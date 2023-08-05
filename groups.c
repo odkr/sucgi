00a0cb12c1be47c8a832fa261700c800df24c232
Odin Kroeger <odkr@users.noreply.github.com>
2023-08-05 13:21:24 +0200

    refactor: factoured out process group check.

---
+++ groups.c
@@ -0,0 +1,101 @@
+/*
+ * Process group verification.
+ *
+ * Copyright 2022 and 2023 Odin Kroeger.
+ *
+ * This file is part of suCGI.
+ *
+ * suCGI is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * suCGI is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with suCGI. If not, see <https://www.gnu.org/licenses>.
+ */
+
+#define _XOPEN_SOURCE 700
+
+#if defined(__OPTIMIZE__) && !defined(_FORTIFY_SOURCE)
+#define _FORTIFY_SOURCE 3
+#endif
+
+#include <sys/types.h>
+#include <assert.h>
+#include <errno.h>
+#include <search.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <syslog.h>
+#include <unistd.h>
+
+#include "groups.h"
+#include "macros.h"
+#include "params.h"
+
+
+bool
+groups_match(size_t ngroups, const gid_t *const groups)
+{
+    gid_t pgroups[MAX_NGROUPS];
+    int npgroups;
+
+    assert(ngroups <= MAX_NGROUPS);
+
+    npgroups = getgroups(MAX_NGROUPS, pgroups);
+    if (npgroups < 0) {
+        /* RATS: ignore; format string is short and a literal. */
+        syslog(LOG_ERR, "number of process groups exceeds MAX_NGROUPS.");
+        return false;
+    }
+
+    if ((size_t) npgroups > ngroups) {
+        /* RATS: ignore; format string is short and a literal. */
+        syslog(LOG_ERR, "process groups not a subset of expected groups.");
+        return false;
+    }
+
+    for (int i = 0; i < npgroups; ++i) {
+        void *ptr;
+
+        ptr = lfind(&pgroups[i], groups, &ngroups, sizeof(gid_t),
+                    (int (*)(const void *, const void *)) groups_compare);
+        if (ptr == NULL) {
+            if (ISSIGNED(gid_t)) {
+                /* RATS: ignore; format string is short and a literal. */
+                syslog(LOG_ERR, "process belongs to group %lld.",
+                       (long long) pgroups[i]);
+            } else {
+                /* RATS: ignore; format string is short and a literal. */
+                syslog(LOG_ERR, "process belongs to group %llu.",
+                       (unsigned long long) pgroups[i]);
+            }
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int
+groups_compare(const gid_t *const gid1, const gid_t *const gid2)
+{
+    assert(gid1 != NULL);
+    assert(gid2 != NULL);
+
+    if (*gid1 < *gid2) {
+        return -1;
+    }
+
+    if (*gid1 > *gid2) {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ groups.h
@@ -0,0 +1,52 @@
+/*
+ * Header for groups.c.
+ *
+ * Copyright 2022 and 2023 Odin Kroeger.
+ *
+ * This file is part of suCGI.
+ *
+ * suCGI is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * suCGI is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with suCGI. If not, see <https://www.gnu.org/licenses>.
+ */
+
+
+/* FIXME: Docs and unit-testing. */
+
+#if !defined(GROUPS_H)
+#define GROUPS_H
+
+#include <sys/types.h>
+#include <stdbool.h>
+
+#include "attr.h"
+
+/*
+ * Check whether only the given secondary groups are set.
+ *
+ * Side-effects:
+ *      Logs groups that should not be set to the system log as error.
+ *
+ * FIXME: Not unit-tested.
+ */
+_read_only(2, 1) _nonnull(2) _nodiscard _unused
+bool groups_match(size_t ngroups, const gid_t *groups);
+
+/*
+ * Check whether two GIDs are the same.
+ *
+ * FIXME: Not unit-tested.
+ */
+_read_only(1) _read_only(2) _nonnull(1, 2) _nodiscard _unused
+int groups_compare(const gid_t *gid1, const gid_t *gid2);
+
+#endif /* !defined(GROUPS_H) */
